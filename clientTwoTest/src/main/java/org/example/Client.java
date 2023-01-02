cfe3462634096f78ed008d2f2d900c3afff7b56a
AndrewEnot <103563559+AndrewEnot@users.noreply.github.com>
2023-01-02 16:05:19 +0200

    Attempt 1

---
+++ clientTwoTest/src/main/java/org/example/Client.java
@@ -0,0 +1,75 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+/*
+ * @author Oksiuta Andrii
+ * 30.12.2022
+ * 11:53
+ */
+public class Client {
+
+  public static void main(String[] args) {
+    int port = 8080;
+
+    try (Socket clientSocket = new Socket("localhost", port);
+        ObjectInputStream inStream = new ObjectInputStream(clientSocket.getInputStream());
+        ObjectOutputStream outStream = new ObjectOutputStream(clientSocket.getOutputStream())) {
+
+      Thread intThread = new Thread(() -> {
+        try {
+          Scanner textIn = new Scanner(System.in);
+
+          while (clientSocket.isConnected()) {
+            System.out.print("This is client 2: Input a command... ");
+            String text = textIn.next();
+            System.out.println("You have entered: " + text);
+
+            switch (text) {
+              case "-exit":
+                outStream.writeObject(text);
+                System.out.println("You ending session. Have a nice day!!!");
+                System.exit(111);
+              case "-file":
+                System.out.print("Please, input path of file you are sending: ");
+                String path = textIn.next();
+                System.out.println("File sending");
+                outStream.writeObject("File receiving...");
+                File file = new File(path);
+                char[] chars = new char[(int) file.length()];
+                try (FileReader reader = new FileReader(file)) {
+                  int read = reader.read(chars);
+                  if (read == -1) {
+                    return;
+                  }
+                  outStream.writeObject(chars);
+                }
+                break;
+              default:
+                System.out.println("Incorrect command...(");
+                break;
+            }
+          }
+
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      });
+      intThread.start();
+
+
+      while (clientSocket.isConnected()) {
+        System.out.println(inStream.readObject());
+      }
+
+    } catch (IOException | ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ clientTwoTest/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+/*
+ * @author Oksiuta Andrii
+ * $DATE
+ * $TIME
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+}
